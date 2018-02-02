ec9ccb6e23ba9858c58b717b526b1f5847ec2eb2
Jugador <dm.evilart@gmail.com>
2018-02-02 13:25:42 +0100

    primer commit

---
+++ src/main/java/EjercicioR207.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class EjercicioR207
+{
+  public String devolverEnOrden(ArrayList<String> palabras)
+  {
+    // Escribe tu código a continuación...
+    String cadenaADevolver = "";
+    if(palabras.size() > 0) {
+        ArrayList<String> copiaPalabras =new ArrayList<> (palabras);
+        for(int i = 0; i < copiaPalabras.size(); i = 0) {
+            String palabraMasLarga = "";
+            for(int j = 0; j < copiaPalabras.size(); j++) {
+                String palabraActual = copiaPalabras.get(j);
+                if(palabraActual.length() >= palabraMasLarga.length()) {
+                    palabraMasLarga = palabraActual;
+                }
+            }
+            copiaPalabras.remove(palabraMasLarga);
+            cadenaADevolver =  cadenaADevolver + " " + palabraMasLarga;
+        }
+        cadenaADevolver = cadenaADevolver.trim();
+        cadenaADevolver = cadenaADevolver.replace(" ",",");
+    }
+    return cadenaADevolver;
+  }
+  
+  // A continuación puedes escribir otros métodos si lo necesitas...
+}
